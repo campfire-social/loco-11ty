a472cbe963efa23ea2ea27e38219434b95da2682
Nathan Skinner <nathan.skinner@bmcjax.com>
2022-06-09 20:40:41 -0400

    adding sass

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,25 @@
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [300, 600],
+    formats: ["webp", "jpg", "png"],
+    outputDir: "./src/assets/images",
+    urlPath: "/assets/images/"
+
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
 module.exports = function (config) {
 
     config.setDataDeepMerge(true);
@@ -6,9 +28,20 @@ module.exports = function (config) {
         dynamicPartials: true 
     });
 
+    config.setBrowserSyncConfig({
+		files: './dist/assets/styles/**/*.css'
+	});
+
     config.addPassthroughCopy({
         "./src/favicon" : "favicon",
+        "./src/fonts" : "assets/fonts",
+        "./src/images" : "assets/images",
+        "./src/css" : "assets/styles"
     });
+
+    config.addNunjucksAsyncShortcode("image", imageShortcode);
+    config.addLiquidShortcode("image", imageShortcode);
+    config.addJavaScriptFunction("image", imageShortcode);
 
     return {
       dir: {
