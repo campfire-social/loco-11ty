f0994425fda7d60b32dbc93e78c38093bf26712b
Nathan Skinner <nathan.skinner@bmcjax.com>
2022-06-29 20:30:34 -0400

    homepage

--- src/js/main.js
+++ src/js/main.js
@@ -8,10 +8,20 @@ document.addEventListener('DOMContentLoaded', function () {
         var splideElement = elms[i];
 
         var splideOptions = {
-            type   : 'loop',
+        
+            arrows: false,
+            pagination: false,
             perPage: 3,
             perMove: 1,
-            arrowPath: 'M0.7,17.6v4.9H30L16.6,35.9l3.5,3.5L39.4,20L20,0.6l-3.5,3.5L30,17.6H0.7z',
+            mediaQuery: 'max',
+            breakpoints: {
+                980: {
+                    type   : 'loop',
+                    arrows: true,
+                    arrowPath: 'M0.7,17.6v4.9H30L16.6,35.9l3.5,3.5L39.4,20L20,0.6l-3.5,3.5L30,17.6H0.7z',
+                   perPage: 1
+                }
+            }
         };
 
         var splide = new Splide( splideElement, splideOptions );
